1eaa280d0c09da1c0a6717493ffd6a6497047bf1
Weichi Zhao <wzhao42@zhejianglab.com>
2023-02-24 16:40:35 +0800

    [bot] add window capture func

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,22 @@
 /*
 Copyright © 2023 NAME HERE <EMAIL ADDRESS>
-
 */
+
 package cmd
 
 import (
 	"os"
 
 	"github.com/spf13/cobra"
+
+	"wz.com/self-learning/pkg/utilities"
 )
-
-
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:   "self-learning",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
-	// Uncomment the following line if your bare application
-	// has an action associated with it:
-	// Run: func(cmd *cobra.Command, args []string) { },
+	Use: "self-learning",
+	//Short: "bot",
+	//Long:  `bot`,
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -46,6 +38,6 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+
+	utilities.Capture("")
 }
-
-
